30f41ceaa440ae173ee7c47fc38835a1aefbf172
VeriTas-arch <1136487701@qq.com>
2024-02-18 18:18:08 +0800

    modified:   HexaLattice.py 	new file:   __pycache__/beam.cpython-38.pyc 	new file:   __pycache__/node.cpython-38.pyc 	new file:   __pycache__/operations.cpython-38.pyc 	modified:   __pycache__/settings.cpython-38.pyc 	modified:   beam.py 	modified:   node.py 	modified:   operations.py 	modified:   settings.py

--- HexaLattice.py
+++ HexaLattice.py
@@ -3,6 +3,7 @@ import pygame
 import pymunk
 import pymunk.pygame_util
 import math
+import numpy as np
 
 from settings import Settings
 from beam import Beam
@@ -11,6 +12,7 @@ from operations import Operations
 
 class HexaLattice:
     """Main class for HexaLattice simulation"""
+
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
@@ -24,8 +26,16 @@ class HexaLattice:
         self.space.gravity = self.settings.gravity
         self.draw_option = pymunk.pygame_util.DrawOptions(self.screen)
 
-        self.node._create_nodes()
-        self.beam._create_beams()
+        self.beam = Beam(self)
+        self.node = Node(self)
+        self.operations = Operations(self)
+
+        self.node_list = []
+        self.init_pos = []
+        self.length = 12
+
+        self._create_nodes(self.space)
+        self._create_beams()
         
         self.running = True
 
@@ -33,6 +43,7 @@ class HexaLattice:
         while self.running:
             self._check_events()
             self._update_screen()
+            self.space.step(1)
             self.clock.tick(self.settings.fps)
 
     def _check_events(self):
@@ -45,8 +56,71 @@ class HexaLattice:
     def _update_screen(self):
         """Update the screen"""
         self.screen.fill(self.settings.bg_color)
+        self.space.debug_draw(self.draw_option)
+
+        # apply force to the nodes
+        body_1 = self.node_list[0]
+        body_2 = self.node_list[1]
+        (F1_x , F1_y) = (60, 80)
+        (F2_x , F2_y) = (0, 100)
+        self.operations.add_force(body_1, F1_x, F1_y)
+        self.operations.add_force(body_2, F2_x, F2_y)
+
+        # draw the arrow
+        self.operations.draw_arrow(body_1.position, body_1.position + (F1_x * 0.5, F1_y * 0.5))
+        self.operations.draw_arrow(body_2.position, body_2.position + (F2_x * 0.5, F2_y * 0.5))
+
+        self.operations.draw_arrow(self.init_pos[10], self.node_list[10].position, "green")
+        self.operations.draw_arrow(self.init_pos[11], self.node_list[11].position, "green")
 
         pygame.display.flip()
+
+    # function that initializes the beams
+    def _create_beams(self):
+        length = self.length
+        notion_mat = np.zeros((length, length))
+        
+        for i in range(length):
+            for j in range (i+1, length):
+                if i%5 != 2 and i%5 != 4:
+                    if j == i + 2 or j == i + 3 or j == i + 5:
+                        notion_mat[i][j] = 1
+                        self.beam.add_beam(self.node_list[i], self.node_list[j], 10)
+
+                elif i%5 == 2 and j == i + 3:
+                        notion_mat[i][j] = 1
+                        self.beam.add_beam(self.node_list[i], self.node_list[j], 10)
+
+                elif i%5 == 4 and j == i + 2:
+                        notion_mat[i][j] = 1
+                        self.beam.add_beam(self.node_list[i], self.node_list[j], 10)
+
+        return notion_mat
+
+    # function that initializes the nodes
+    def _create_nodes(self, space):
+        # calculate the position bias of the nodes, note that the size of the screen is not called automatically
+        sep_h = (720 - 100) / 2
+        sep_w = (1080 - 4 * 100 * math.sin(math.pi/3)) / 2
+
+        for i in range(0,12):
+            if i%5 == 0 or i%5 == 1:
+                pos_x = sep_w + 100 * math.sin(math.pi/3) + (i%5) * 2 * 100 * math.sin(math.pi/3)
+                pos_y = sep_h - 100 * math.cos(math.pi/3) + 100 * (i - i%5) / 5
+                self.node_list.append(self.node.add_float_node(space, 20, 10,(pos_x, pos_y)))
+                self.init_pos.append((pos_x, pos_y))
+
+            elif i%5 == 3:
+                pos_x = sep_w + 2 * 100 * math.sin(math.pi/3)
+                pos_y = sep_h + 100 * (i - 3) / 5
+                self.node_list.append(self.node.add_float_node(space, 20, 10, (pos_x, pos_y)))
+                self.init_pos.append((pos_x, pos_y))
+
+            elif i%5 == 2 or i%5 == 4:
+                pos_x = sep_w + (i%5 - 2) * 2 * 100 * math.sin(math.pi/3)
+                pos_y = sep_h + 100 * (i - i%5) / 5
+                self.node_list.append(self.node.add_static_node(space, 20, (pos_x, pos_y)))
+                self.init_pos.append((pos_x, pos_y))
 
 
 if __name__ == '__main__':
--- beam.py
+++ beam.py
@@ -1,12 +1,21 @@
+import pymunk
+import pymunk.pygame_util
+
 class Beam:
     """class for creating beams in the simulation"""
     
-    def __init__(self):
-        self.damping = 10
+    def __init__(self, me_network):
+        self.space = me_network.space
+        self.settings = me_network.settings
+        self.damping = self.settings.damping
 
     # fuction that adds a spring (i.e. beam)
     def add_beam(self, body_1, body_2, stiffness):
         damping = self.damping
-        spring = pymunk.DampedSpring(body_1, body_2, (0, 0), (0, 0), 0, stiffness, damping)
+        spring = pymunk.DampedSpring(body_1, body_2, (0, 0), (0, 0), 100, stiffness, damping)
         
-        space.add(spring)
+        self.space.add(spring)
+
+
+
+
