b190c7fb6e8040a8da804b2b9b98d21d66a62906
VeriTas-arch <145042207+VeriTas-arch@users.noreply.github.com>
2024-06-28 19:26:27 +0800

    updated the comments of EVA.py

--- EVA.py
+++ EVA.py
@@ -1,6 +1,7 @@
 from settings import Settings
 import math
 import numpy as np
+from typing import Tuple
 
 set = Settings()
 
@@ -18,6 +19,9 @@ POP_SIZE = set.POP_SIZE
 DNA_SIZE = set.DNA_SIZE
 MUTATION_RATE = set.MUTATION_RATE
 
+Array_Individual = np.ndarray[Tuple[int, int], np.float64]
+Array_Population = np.ndarray[Tuple[int, int, int], np.float64]
+
 
 def fillBits(size):
     return 1 << size - 1
@@ -25,19 +29,6 @@ def fillBits(size):
 
 def target_function():
     """set sine function as target function"""
-    """
-    # define the constants
-    blen = set.beam_length
-    sep_x = (set.screen_width - (set.row_lenh - 1) * blen * math.sqrt(3)) / 2
-    sep_y = (set.screen_height - ((set.row_num - 1) / 2) * blen) / 2
-
-    # define the sine function related parameters
-    T = set.beam_length * 2 * math.sqrt(3) * 1.02
-    omiga = 2 * math.pi / T
-    Amp = set.beam_length / 25
-    bias = set.screen_height - sep_y + 12
-    return lambda x: Amp * math.sin(omiga * (x - sep_x) + math.pi / 2) + bias
-    """
 
     # define the constants
     blen = set.beam_length
@@ -103,8 +94,22 @@ def get_fitness(indPos):
     return fitness
 
 
-def select_parent(pop, fitness):
-    """choose the parent based on fitness"""
+def select_parent(
+    pop: Array_Population,
+    fitness: np.ndarray[int, np.float64],
+) -> Array_Population:
+    """
+    Selects a parent from the population based on fitness.
+
+    Args:
+        pop (Array_Population): The population of individuals.
+        fitness (np.ndarray[int, np.float64]): The fitness values of the individuals.
+
+    Returns:
+        Array_Population: The selected parent.
+
+    """
+
     parent_index = np.random.choice(
         POP_SIZE, size=POP_SIZE, replace=True, p=fitness / sum(fitness)
     )
@@ -112,28 +117,28 @@ def select_parent(pop, fitness):
     return [pop[parent_index[i]] for i in range(POP_SIZE)]
 
 
-def crossover(pending_pop, parent, crossover_rate, fitness, type=0):
-    """crossover the parents to generate offspring"""
+def crossover(
+    pending_pop: Array_Population,
+    parent: Array_Individual,
+    crossover_rate: float,
+    fitness: np.ndarray[int, np.float64],
+    type=0,
+) -> Array_Individual:
+    """
+    Perform crossover operation between parent and pending population.
+
+    Args:
+        pending_pop (Array_Population): The pending population.
+        parent (Array_Individual): The parent individual.
+        crossover_rate (float): The probability of performing crossover.
+        fitness (np.ndarray[int, np.float64]): The fitness values of the population.
+        type (int, optional): The type of crossover operation. Defaults to 0.
+
+    Returns:
+        Array_Individual: The resulting individual after crossover.
+    """
+
     if np.random.rand() < crossover_rate:
-
-        # index = np.random.choice(POP_SIZE, p=fitness / sum(fitness))
-        # point = np.random.randint(1, DNA_SIZE - 1)
-
-        # if type == 0:
-        #     crossover_result = [
-        #         pop[index][i] if abs(i - point) < 2 else parent[i]
-        #         for i in range(node_num)
-        #     ]
-
-        # if type == 1:
-        #     crossover_result = [
-        #         np.concatenate((parent[i][:point], pop[index][i][point:]))
-        #         for i in range(node_num)
-        #     ]
-
-        # return crossover_result
-
-        # temp = pop.copy()
         point_num = int(DNA_SIZE / 10) + 1
         point = np.random.choice(
             np.arange(1, DNA_SIZE - 1), replace=False, size=point_num
@@ -141,13 +146,11 @@ def crossover(pending_pop, parent, crossover_rate, fitness, type=0):
         cross_index = np.random.choice(POP_SIZE, p=fitness / sum(fitness))
 
         for k in range(point_num):
-
             if type == 0:
                 pending_pop = [
                     pending_pop[cross_index][i] if abs(i - point[k]) < 2 else parent[i]
                     for i in range(node_num)
                 ]
-
             if type == 1:
                 pending_pop = [
                     np.concatenate(
@@ -157,15 +160,23 @@ def crossover(pending_pop, parent, crossover_rate, fitness, type=0):
                 ]
 
             return pending_pop
-
     else:
         return parent
 
 
-def mutate(child, mutation_rate):
-    """mutation operator"""
+def mutate(child: Array_Individual, mutation_rate: float) -> Array_Individual:
+    """Mutation operator.
+
+    Args:
+        child (Array_Individual): The individual to be mutated.
+        mutation_rate (float): The probability of mutation.
+
+    Returns:
+        Array_Individual: The mutated individual.
+    """
+
     if np.random.rand() < mutation_rate:
-        child = np.array(child)
+        # child = np.array(child)
         interval = np.max(child) - np.min(child)
         point = np.random.randint(0, DNA_SIZE)
 
@@ -177,8 +188,27 @@ def mutate(child, mutation_rate):
     return child
 
 
-def process(pending_pop, parent, crossover_rate, mutation_rate, fitness):
-    """process the population with crossover and mutation"""
+def process(
+    pending_pop: Array_Population,
+    parent: Array_Individual,
+    crossover_rate: float,
+    mutation_rate: float,
+    fitness: np.ndarray[int, np.float64],
+) -> Array_Individual:
+    """
+    Process the pending population by performing crossover and mutation operations.
+
+    Args:
+        pending_pop (Array_Population): The pending population of individuals.
+        parent (Array_Individual): The parent individual used for crossover.
+        crossover_rate (float): The rate at which crossover is performed.
+        mutation_rate (float): The rate at which mutation is performed.
+        fitness (np.ndarray[int, np.float64]): The fitness values of the individuals.
+
+    Returns:
+        Array_Individual: The offspring population after performing crossover and mutation.
+    """
+
     offspring = crossover(pending_pop, parent, crossover_rate, fitness)
     offspring = mutate(offspring, mutation_rate)
 
